f6d8cbb17bef122cb2265fae5febf9f14649c330
Arnab Kumar Nandy <arnab.nandycofinity-x.com>
2024-01-19 12:05:24 +0100

    feat: Compare this snippets

--- src/main/java/org/eclipse/tractusx/selfdescriptionfactory/service/SDFactory.java
+++ src/main/java/org/eclipse/tractusx/selfdescriptionfactory/service/SDFactory.java
@@ -4,3 +4,6 @@ package org.eclipse.tractusx.selfdescriptionfactory.service;
 public interface SDFactory {
     void createVC(Object document);
 }
+
+
+// Compare this snippet from src/main/java/org/eclipse/tractusx/selfdescriptionfactory/service/SDFactory.java
